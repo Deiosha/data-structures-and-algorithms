84f7d06003be690632a90f30f342ae11d3926746
DEIOSHA SPARKS <DEIOSHASPARKS13@GMAIL.COM>
2023-04-01 15:50:33 -0700

    pushing correct test

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,15 +1,48 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class LinkedList:
     """
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, head=None):
+        self.head = head
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, value):
+        old_head = self.head
+        self.head = Node(value)
+        self.head.next = old_head
+
+    def __str__(self):
+        if self.head is None:
+            return "NULL"
+
+    def __str__(self):
+        present = self.head
+        new_val = []
+        while present is not None:
+            new_val.append(str(present.value))
+            present = present.next
+        return " -> ".join(["{ "+value+" }" for value in new_val] + ["NULL"])
+
+    def includes(self, value):
+        present = self.head
+        new_val = []
+
+        while present is not None:
+            new_val.append(str(present.value))
+
+        if value in present:
+            return True
+        else:
+            return False
+
+
+
 
 
 class TargetError:
