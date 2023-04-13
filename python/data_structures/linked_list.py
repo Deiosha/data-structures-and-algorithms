ae20fe2926bda4d2f1425548eec963ed9538c713
DEIOSHA SPARKS <DEIOSHASPARKS13@GMAIL.COM>
2023-04-13 09:34:30 -0700

    finished Code Challenge: Class 07

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,110 +1,152 @@
-class Node:
-    def __init__(self, value, next=None):
-        self.value = value
-        self.next = next
-
-
 class LinkedList:
     """
-    Put docstring here
+    Pass in a head node to implement a linked list.
     """
 
     def __init__(self, head=None):
+        # initialization here
         self.head = head
-        pass
 
     def insert(self, value):
-        old_head = self.head
-        self.head = Node(value)
-        self.head.next = old_head
+        node = Node(value)
+        if self.head is not None:
+            node.next_ = self.head
+        self.head = node
 
     def __str__(self):
-        current_value = self.head
-        ll_list = []
-        while current_value is not None:
-            ll_list.append(str(current_value.value))
-            current_value = current_value.next
-        return " -> ".join(["{ " + value + " }" for value in ll_list] + ["NULL"])
+        current = self.head
+        printout = ''
+        while current is not None:
+            printout += f'{{ {current.value} }} -> '
+            current = current.next_
+        printout += f'NULL'
+        return printout
 
     def includes(self, value):
-
-        current = self.head
-
-        while current:
-            if current.value == value:
-                return True
-            current = current.next
-        return False
-
-    def append(self, new_value):
-
-        current = self.head
-        new_node = Node(new_value)
-
-        if current is None:
-            self.head = new_node
-            return
-
-        while current.next:
-            current = current.next
-
-        current.next = new_node
-
-    def insert_before(self, target_value, new_value):
-        if self.head is None:
-            raise TargetError("invalid value: " + target_value)
-
-        if self.head.value == target_value:
-            self.insert(new_value)
-            return
-
-        current = self.head
-        while current.next is not None:
-            if current.next.value == target_value:
-                new_node = Node(new_value)
-                new_node.next = current.next
-                current.next = new_node
-                return
-            current = current.next
-
-        raise TargetError("invalid value: " + target_value)
-
-    def insert_after(self, target_value, new_value):
-        if self.head is None:
-            raise TargetError(target_value + " nonexistent")
-
         current = self.head
         while current is not None:
-            if current.value == target_value:
-                new_node = Node(new_value)
-                new_node.next = current.next
-                current.next = new_node
-                return
-            current = current.next
+            if current.value is value:
+                return True
+            current = current.next_
+        return False
 
-        raise TargetError(target_value + " nonexistent")
+    def append(self, value):
+        node = Node(value)
+
+        if self.head is None:
+            self.head = node
+        else:
+            current = self.head
+            while current.next_:
+                current = current.next_
+            current.next_ = node
+
+    def insert_before(self, value, new_value):
+        if self.head is None:
+            raise TargetError('Empty list')
+
+        if not self.includes(value):
+            raise TargetError('Value not found')
+
+        if self.head.value is value:
+            node = Node(new_value, self.head)
+            self.head = node
+        else:
+            current = self.head
+            while current:
+                if current.next_.value is value:
+                    node = Node(new_value, current.next_)
+                    current.next_ = node
+                    break
+                else:
+                    current = current.next_
+
+    def insert_after(self, value, new_value):
+        if self.head is None:
+            raise TargetError('Empty list')
+
+        if not self.includes(value):
+            raise TargetError('Value not found')
+
+        current = self.head
+        while current:
+            if current.value is value:
+                node = Node(new_value, current.next_)
+                current.next_ = node
+                break
+            else:
+                current = current.next_
 
     def kth_from_end(self, k):
         length = 0
         current = self.head
-        counter = 0
+
         while current:
             length += 1
-            current = current.next
-        if k > length:
-            raise Exception
-        else:
-            counter = length - k
-        current = self.head
-        while counter - 1:
-            counter -= 1
-            current = current.next
-        return current.value
-#
+            current = current.next_
 
+        target = length - k
+
+        if k >= length:
+            raise TargetError('k exceeds list length')
+
+        if k < 0:
+            raise TargetError('k must be a positive integer')
+
+        if target == 0:
+            return self.head.value
+
+        current = self.head
+
+        for i in range(target - 1):
+            current = current.next_
+
+        return current.value
+
+    def reverse(self):
+        if not self.head:
+            return None
+
+        prev = None
+        curr = self.head
+
+        while curr:
+            next_node = curr.next_
+            curr.next_ = prev
+            prev = curr
+            curr = next_node
+
+        self.head = prev
+
+    def add_one(self):
+        self.reverse()
+        curr = self.head
+
+        while curr:
+            if curr.value == 9 and curr.next_:
+                curr.value = 0
+                curr = curr.next_
+
+            elif curr.value < 9:
+                curr.value += 1
+                break
+
+            else:
+                curr.value = 0
+                curr.next_ = Node(1)
+                break
+        self.reverse()
+
+
+class Node:
+    def __init__(self, value, next_=None):
+        self.value = value
+        self.next_ = next_
 
 
 class TargetError(Exception):
-    pass
+    def __init__(self, message):
+        self.message = message
 
-
+    def __str__(self):
+        return self.message
