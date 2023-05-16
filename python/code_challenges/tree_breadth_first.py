c63ab7d93742723385e80576e71f8bb6a6e56841
DEIOSHA SPARKS <DEIOSHASPARKS13@GMAIL.COM>
2023-05-15 19:36:11 -0700

    finished + readme

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,32 @@
 from data_structures.binary_tree import BinaryTree
 
 
-def breadth_first():
-    pass
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree:
+    def __init__(self, root=None):
+        self.root = root
+
+
+def breadth_first(tree):
+    if tree.root is None:
+        return []
+
+    queue = [tree.root]
+    traversal = []
+
+    while len(queue) > 0:
+        node = queue.pop(0)
+        traversal.append(node.value)
+
+        if node.left is not None:
+            queue.append(node.left)
+        if node.right is not None:
+            queue.append(node.right)
+
+    return traversal
